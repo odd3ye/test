2e3b19226ffa8aada3be50ccb5c3b646e31b7bbc
odd3ye <103198048+odd3ye@users.noreply.github.com>
2022-08-01 21:30:55 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let colors = [
+    {
+        id: 0,
+        color: "yellow",
+        title: "Hyunjin",
+        description: "HyunJin (Hangul: 현진) is the second revealed member of LOONA and a member of its first sub-unit, LOONA 1/3. She was born as Kim Hyun-jin (Hangul: 김현진) on November 15, 2000 in Dunchon-dong, Gangdong District, Seoul, South Korea."
+    },
+    {
+        id: 1,
+        color: "green",
+        title: "Haseul",
+        description: "HaSeul (Hangul: 하슬) is the third revealed member of LOONA and a member of its first sub-unit, LOONA 1/3. She was born as Cho Ha-seul (Hangul: 조하슬) on August 18, 1997, in Suncheon, South Jeolla Province, South Korea."
+    }
+]
+
+colors.map(function(val, index){
+    const {id, color, title, description} = val;
+    document.querySelector(".wrapper").innerHTML += `<div class="content" style="background: ${color};">
+                <h1>${title}</h1>
+                <p>${description}</p>
+            </div>`
+})
